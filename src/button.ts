b9c1dcc67b970a6cffc6c936a2429ecb18946154
James Richford <jamesrichford@googlemail.com>
2016-05-21 01:02:31 +0100

    Add button type

--- src/button.ts
+++ src/button.ts
@@ -4,10 +4,11 @@ import { NgClass } from "angular2/common";
 @Component({
     directives: [NgClass],
     selector: "feel-button",
-    template: "<button type='button' [ngClass]=\"{ 'btn-flat': flat, 'btn-primary': primary }\" >{{text}}</button>"
+    template: `<button [type]=type [ngClass]=\"{ 'btn-flat': flat, 'btn-primary': primary }\" >{{text}}</button>`
 })
 export class ButtonComponent {
 
+   @Input() type: string = "button";
    @Input() primary: boolean;
    @Input() flat: boolean;
    @Input() text: string;
