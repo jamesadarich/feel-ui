63e0a586df90f061f0941e39d61e9a8bcf470e4b
James Richford <jamesrichford@googlemail.com>
2017-04-10 23:57:41 +0100

    transform button to angular 2

--- src/components/button.ts
+++ src/components/button.ts
@@ -1,8 +1,6 @@
-import { Component, Input} from "angular2/core";
-import { NgClass } from "angular2/common";
+import { Component, Input} from "@angular/core";
 
 @Component({
-    directives: [NgClass],
     selector: "feel-button",
     template: `<button [type]=type [ngClass]=\"{ 'btn-flat': flat, 'btn-raised': !flat, 'btn-primary': primary }\" >{{text}}</button>`
 })
---
+++ src/feel-ui.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from "@angular/core";
+import { ButtonComponent } from "./components/button";
+
+@NgModule({
+    declarations: [ ButtonComponent ]
+})
+export class FeelUiModule {};
